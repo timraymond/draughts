0c6a32746ccd4bf4637c24f2e7f2127b3613f348
Tim Raymond <xtjraymondx@gmail.com>
2012-08-11 13:49:54 -0400

    Synchronous AI processing
    I need to move this into a background process. Most likely with
    beanstalk

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,6 +13,15 @@ class GamesController < ApplicationController
     @game.state = engine.to_s
     @game.save
 
+    ai = ::Checkers::AI.new(::Checkers::Game.from_text(engine.to_s))
+    resp = ai.choose
+    move_src, move_dest = resp.to_s.split('x')
+    puts "src: #{move_src}, dest: #{move_dest}"
+    move = engine.options.select { |move| move.src == move_src.to_i && move.dest == move_dest.to_i }.first
+    engine.commit_move(move)
+    @game.state = engine.to_s
+    @game.save
+
     redirect_to :action => "show", :id => 0
   end
 end
