4606e2a69edbfbd0c2fa39172c07f3ae9d32b645
Tim Raymond <xtjraymondx@gmail.com>
2012-08-11 11:07:40 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Draughts::Application.config.secret_token = '582180c158dea9d51bfdc7cf907624d60c06a896e003b67e9ffbdb194a5fe1f26aaac0ccfcc4ac66c508a0b2d51294afd41c44f9575008a073918289550e2455'
