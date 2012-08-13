8bae24dc84c0b8257de9fdc87e0ac7ce7c45165b
Tim Raymond <xtjraymondx@gmail.com>
2012-08-13 15:55:25 -0400

    nginx config for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,3 +17,23 @@ ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
+namespace :deploy do
+  task :restart, roles: :app, except: {no_release: true} do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
